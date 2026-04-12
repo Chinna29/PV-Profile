3d75666dfca7bee2f4bb7baf30b5eb1d4402b209
PV <creatorsputy@gmail.com>
2026-04-12 15:44:00 +0530

    Added the blog tag Updated the about and portfolio folders

--- forms/contact.php
+++ forms/contact.php
@@ -1,43 +1,24 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+require 'PHPMailer/PHPMailer.php';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'creatorsputy@gmail.com';
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'codebusters8080@gmail.com';
+$mail->Password = 'dtz pscc imyc crsz';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$mail->setFrom($_POST['email'], $_POST['name']);
+$mail->addAddress('codebusters8080@gmail.com');
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$mail->Subject = $_POST['subject'];
+$mail->Body = $_POST['message'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  //SMTP-PV
-  //tdtz pscc imyc crsz
-
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'codebusters8080',
-    'password' => 'tdtz pscc imyc crsz',
-    'port' => '587'
-  );
- 
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+if (!$mail->send()) {
+    echo "Error: " . $mail->ErrorInfo;
+} else {
+    echo "Email sent!";
+}
 ?>
